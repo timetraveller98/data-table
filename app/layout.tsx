616089af39e2b10a8c0587e2818bd8c2c6f98d45
Paradox <chuhan718@gmail.com>
2024-12-22 10:39:31 +0530

    upload dara

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
 import "./globals.css";
-
-// If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- components/data-table/ProjectManager.tsx
+++ components/data-table/ProjectManager.tsx
@@ -14,13 +14,13 @@ const ProjectManager = ({ name }: { name: string }) => {
       <Tooltip delayDuration={100}>
         <TooltipTrigger>
           <Avatar className="w-7 h-7 rounded-md">
-            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarImage src="/ajay.jpg" />
             <AvatarFallback>
               {name[0]} {name.split(" ")[1][0]}
             </AvatarFallback>
           </Avatar>
         </TooltipTrigger>
-        <TooltipContent side="bottom" className="bg-gray-900 text-base">
+        <TooltipContent side="bottom" className="bg-gray-900 text-white">
           <Arrow />
           {name}
         </TooltipContent>
